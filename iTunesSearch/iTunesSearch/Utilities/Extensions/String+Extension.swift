f6875cd909a4cd8daf9433fd0078d33c4a3a6f4c
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2022-05-29 12:39:17 +0300

    Add string extension

---
+++ iTunesSearch/iTunesSearch/Utilities/Extensions/String+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  String+Extension.swift
+//  iTunesSearch
+//
+//  Created by Enes Buğra Yenidünya on 29.05.2022.
+//
+
+import Foundation
+
+extension String {
+    static var empty: String {
+        ""
+    }
+}
