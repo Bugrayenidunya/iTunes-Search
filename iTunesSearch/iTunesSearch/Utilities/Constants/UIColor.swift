c3650c964bf5a8c17f3e0b42f95910300b8f6b28
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2022-05-29 10:15:20 +0300

    Add constants

---
+++ iTunesSearch/iTunesSearch/Utilities/Constants/UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor.swift
+//  iTunesSearch
+//
+//  Created by Enes Buğra Yenidünya on 29.05.2022.
+//
+
+import Foundation
+
+extension Constants {
+    /// Color constants for UIColor extension
+    class UIColor {
+        static let themeColor = "themeColor"
+    }
+}
